7065fda66da2292249c6791ab6a6165601b99372
sour <samuel.lindeyer@gmail.com>
2024-09-23 12:40:47 +1000

    added speed insights

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import Header from './components/Header'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: 'Sour Gecko',
@@ -10,6 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Analytics></Analytics>
+      <SpeedInsights></SpeedInsights>
       <body>
         <Header/>
         <main>{children}</main>
